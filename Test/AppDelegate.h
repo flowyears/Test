ad1dfbcde16facae7adf5fddf766f4622dd45e8c
shenjx <shenjx@shenjxdeMac-mini.local>
2015-05-19 20:32:51 +0800

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by shenjx on 15/5/19.
+//  Copyright (c) 2015年 shenjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by shenjx on 15/5/19.
+//  Copyright (c) 2015年 shenjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
